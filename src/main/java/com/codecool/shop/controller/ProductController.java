07364d05b263aa1d9b614ebb4addb71473bbd75d
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-01-10 11:23:16 +0100

    added filter by supplier

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -24,6 +26,7 @@ public class ProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
 //        Map params = new HashMap<>();
 //        params.put("category", productCategoryDataStore.find(1));
@@ -34,17 +37,22 @@ public class ProductController extends HttpServlet {
 //        context.setVariables(params);
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.getAll());
+        context.setVariable("supplier", supplierDataStore.getAll());
 
-        if (req.getParameterNames().hasMoreElements()) {
-            if (req.getParameter("category").equals("Tablet")) {
+        if (!req.getParameterNames().hasMoreElements() || req.getParameterValues(req.getParameterNames().nextElement())[0].equals("All")) {
+            context.setVariable("products", productDataStore.getAll());
+        } else if (req.getParameterNames().nextElement().equals("category")) {
+            if (req.getParameter("category").equals("Tablet")) { //productCategoryDataStore.getAll().get(0).getName()
                 context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
             } else if (req.getParameter("category").equals("Phone")) {
                 context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(2)));
-            } else {
-                context.setVariable("products", productDataStore.getAll());
             }
-        } else {
-            context.setVariable("products", productDataStore.getAll());
+        } else if (req.getParameterNames().nextElement().equals("supplier")) {
+            if (req.getParameter("supplier").equals("Amazon")) {
+                context.setVariable("products", productDataStore.getBy(supplierDataStore.find(1)));
+            } else if (req.getParameter("supplier").equals("Lenovo")) {
+                context.setVariable("products", productDataStore.getBy(supplierDataStore.find(2)));
+            }
         }
 
         engine.process("product/index.html", context, resp.getWriter());
