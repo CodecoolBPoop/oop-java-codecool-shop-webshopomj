9c65f8da399c20fbe0349b16ca71c3acb9188502
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-01-22 15:04:09 +0100

    created connection for Product & ProductCategory

---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -0,0 +1,98 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+    private static final String DATABASE;
+    private static final String DB_USER;
+    private static final String DB_PASSWORD;
+
+    @Override
+    public void add(ProductCategory category) {
+        String query = "INSERT INTO product_categories (name, department, description)" +
+                "VALUES ('"
+                + category.getName() + "', '"
+                + category.getDepartment() + "', '"
+                + category.getDescription()
+                + "'); '";
+        executeQuery(query);
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        String query = "SELECT * FROM product_categories WHERE id = '" + id + "';";
+
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ) {
+            if (resultSet.next()) {
+                ProductCategory result = new ProductCategory(
+                        resultSet.getString("name"),
+                        resultSet.getString("department"),
+                        resultSet.getString("description")
+                        );
+                return result;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+        String query = "DELETE FROM product_categories WHERE id = '" + id + "';";
+
+        executeQuery(query);
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        String query = "SELECT * FROM product_categories";
+        List<ProductCategory> resultList = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ) {
+            if (resultSet.next()) {
+                ProductCategory result = new ProductCategory(
+                        resultSet.getString("name"),
+                        resultSet.getString("department"),
+                        resultSet.getString("description")
+                );
+                resultList.add(result);
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+        ) {
+            statement.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DATABASE, DB_USER, DB_PASSWORD);
+    }
+
+}
---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -0,0 +1,72 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+
+import java.sql.*;
+
+public class ProductDaoJdbc implements ProductDao {
+    private static final String DATABASE;
+    private static final String DB_USER;
+    private static final String DB_PASSWORD;
+
+    @Override
+    public void add(Product product) {
+        String query = "INSERT INTO products " +
+                "(name, default_price, currency, description, product_category_id, supplier_id)" +
+                "VALUES ('"
+                + product.getName() + "', '"
+                + product.getDefaultPrice() + "', '"
+                + product.getDefaultCurrency() + "', '"
+                + product.getDescription() + "', '"
+                + 1 + "', '"
+                + 1 + "'); '";
+        executeQuery(query);
+    }
+
+    @Override
+    public Product find(int id) {
+        String query = "SELECT * FROM products WHERE id = '" + id + "';";
+
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+        ) {
+            if (resultSet.next()) {
+                Product result = new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getFloat("default_price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        (resultSet.getInt("product_category_id")),
+                        resultSet.getInt("supplier_id"));
+                return result;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+        ) {
+            statement.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DATABASE, DB_USER, DB_PASSWORD);
+    }
+
+}
