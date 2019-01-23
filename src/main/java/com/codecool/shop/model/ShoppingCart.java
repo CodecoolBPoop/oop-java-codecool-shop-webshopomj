9cd7ced447a173e7f75f7de3514d5b11827d8637
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-01-23 16:53:09 +0100

    created ShoppingCartDaoJdbc

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -7,10 +7,19 @@ public class ShoppingCart extends BaseModel {
     public static List<Product> productInShoppingCart = new ArrayList<>();
     public static int numOfItemsInShoppingCart = 0;
 
+    private int productId;
+    private int amount;
+    private float price;
+
 
     public ShoppingCart() {
     }
 
+    public ShoppingCart(int productId, int amount, float price) {
+        this.productId = productId;
+        this.amount = amount;
+        this.price = price;
+    }
 
     public List<Product> getProductInShoppingCart() {
         return productInShoppingCart;
@@ -33,7 +42,9 @@ public class ShoppingCart extends BaseModel {
     @Override
     public String toString() {
         return "ShoppingCart{" +
-                "productInShoppingCart=" + productInShoppingCart +
+                "productId=" + productId +
+                ", amount=" + amount +
+                ", price=" + price +
                 '}';
     }
 }
