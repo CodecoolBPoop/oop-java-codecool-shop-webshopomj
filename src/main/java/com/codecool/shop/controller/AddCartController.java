46cb434ca8a725fd8b26ba51c026fa2d9d0cd499
deaack <deaackben66@gmail.com>
2019-01-24 12:01:27 +0100

    line items displayed by key value pairs

--- src/main/java/com/codecool/shop/controller/AddCartController.java
+++ src/main/java/com/codecool/shop/controller/AddCartController.java
@@ -29,40 +29,30 @@ public class AddCartController extends HttpServlet {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ShoppingCartDao cartDataStore = ShoppingCartDaoMem.getInstance();
 
-
-
-        // From HTML
         int id = Integer.parseInt(req.getParameter("incoming"));
 
-        // products
         Product tofind = productDataStore.find(id);
         System.out.println(tofind.hashCode());
 
-        // add to cart
         cartDataStore.add(tofind);
 
         List<Product> productsincart = cartDataStore.getAll();
 
-        Map<Integer, List<Product>> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (Product student : productsincart) {
             Integer key  = student.getId();
             if(map.containsKey(key)){
-                List<Product> list = map.get(key);
-                list.add(student);
+                Integer idd = map.get(key);
+                map.put(key, idd +1);
 
             }else{
-                List<Product> list = new ArrayList<Product>();
-                list.add(student);
-                map.put(key, list);
+                Integer amount = 1;
+                map.put(key, amount);
             }
-
         }
 
         System.out.println(map);
-
-
-
 
         resp.sendRedirect("index.html");
     }
