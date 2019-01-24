4d5d31009fe69dc420fa0d36b4fc1cbc4db898a2
Dániel Takács <takacs.d24@gmail.com>
2019-01-24 09:45:15 +0100

    Checkout page base working (rooting, base html, link from cart page)

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ShoppingCartDao;
+import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet {
+    @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ShoppingCartDao cartDataStore = ShoppingCartDaoMem.getInstance();
+        TemplateEngine engine3 = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context3 = new WebContext(req, resp, req.getServletContext());
+        context3.setVariable("checkoutCart", cartDataStore.getAll());
+        engine3.process("checkout.html", context3, resp.getWriter());
+
+    }
+}
